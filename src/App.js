33d09e1db70cf066fab589e0a40fb029bb8af800
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-05 00:08:38 +0530

    added settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Home from "./pages/homepage/Home"
 import Topbar from "./components/topbar/TopBar"
 import Single from "./pages/homepage/single/Single";
-
+import Write from "./pages/homepage/write/Write";
+import Settings from "./pages/homepage/settings/Settings";
 function App() {
   return (
     <>
 <Topbar/>
 
-<Single/>
+<Settings/>
 </>
   );
 }
---
+++ src/pages/homepage/settings/Settings.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../../components/sidebar/Sidebar"
+import "./settings.css"
+
+export default function Setting() {
+  return (
+    <div className="settings">
+        <div className="settingsWrapper">    </div>
+        <Sidebar/>
+    
+    </div>
+  )
+}
