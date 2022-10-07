57079f3c34314461c5d12b4d85c3af46d6564e7c
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-07 09:52:31 +0530

    user delete route completed

--- api/routes/users.js
+++ api/routes/users.js
@@ -25,6 +25,32 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+/* --------------------------------- Delete --------------------------------- */
+router.delete("/:id", async (req, res) => {
+    
+  if (req.body.userId == req.params.id) {
+ 
+    try {
+        const user=await User.findById(req.params.id)
+        
+      try {
+     console.log(user.username);
+        await Post.deleteMany({username:user.username})
+      
+        await User.findByIdAndDelete(req.params.id);
+        
+        res.status(200).json("user has been delete ....");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+        res.status(404).json("user not found")
+    }
+  } else {
+    res.status(401).json("you can delete only your account!.");
+  }
+});
+
 
 
 
