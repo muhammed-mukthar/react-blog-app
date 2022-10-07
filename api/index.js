039e338b724cd80c0921f5d6c07ffb91508c323c
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-07 09:51:46 +0530

    user update route added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express =require('express')
 const app=express()
 const dotenv=require('dotenv')
+const logger=require('morgan')
 const mongoose=require('mongoose')
 dotenv.config()
 app.use(express.json())
+app.use(logger("dev"));
 const authroute=require('../api/routes/auth')
+const userroute=require('../api/routes/users')
 
 mongoose.connect(process.env.MONGO_CONNECT,{
     useNewUrlParser: true,
@@ -12,8 +15,9 @@ mongoose.connect(process.env.MONGO_CONNECT,{
     
 }).then(console.log("connected to mongo")).catch((err)=> console.log(err,"not connected to mongo"))
 
-app.use("/api/auth",authroute)
 
+app.use("/api/auth",authroute)
+app.use("/api/users",userroute)
 
 app.listen("5000",()=>{
     console.log("Backend  on port 5000");
