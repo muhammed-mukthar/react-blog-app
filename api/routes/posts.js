8fe9eed00c4ba72821609bf03082f168050d1f60
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-07 10:54:52 +0530

    update post route addded

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,21 +1,44 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
+const { createCollection } = require("../models/User");
 
 /* -------------------------------- Create Post -------------------------------- */
 
 router.post("/", async (req, res) => {
-    const newPost=new Post(req.body)
- try{
-
-    const savedPost=await newPost.save()
-    res.status(200).json(savedPost)
- }catch(err){
-    res.status(500).json(err)
- }
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+/* --------------------------------- Update Post --------------------------------- */
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
+    if (post.username === req.body.username) {
+        
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          { $set: req.body },
+          { new: true }
+        );
+        res.status(200).json(updatedPost)
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
