34c40ea3562d032d83e2e436ce8f54b385cb3206
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-13 18:26:10 +0530

    getting posts using api

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,32 +1,29 @@
 import "./post.css";
-
-export default function Post() {
+import {Link} from 'react-router-dom'
+export default function Post({post}) {
   return (
     <div className="post">
+{post.photo &&(
       <img
         className="postImg"
-        src="https://raw.githubusercontent.com/mdn/learning-area/master/html/multimedia-and-embedding/images-in-html/dinosaur_small.jpg"
+        src={post.photo}
         alt=""
-      />
+      />)}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c)=>(
+            <span className="postCat">{c.name}</span>
+          ))}
+
         </div>
-        <span className="postTitle">Lorem ipsum dolor sr</span>
+        <Link to={`/post/${post._id}`} >
+        <span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">{new Date( post.createdAt).toDateString()}</span>
       </div>
       <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolores
-        minus est quae, omnis, ad natus eveniet dolorum, officia laboriosam quod
-        optio ea non doloremque porro? Natus maiores veritatis architecto.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolores
-        minus est quae, omnis, ad natus eveniet dolorum, officia laboriosam quod
-        optio ea non doloremque porro? Natus maiores veritatis architecto.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolores
-        minus est quae, omnis, ad natus eveniet dolorum, officia laboriosam quod
-        optio ea non doloremque porro? Natus maiores veritatis architecto.
+        {post.desc}
       </p>
     </div>
   );
--- client/src/pages/homepage/Home.jsx
+++ client/src/pages/homepage/Home.jsx
@@ -2,12 +2,25 @@ import "./home.css"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from 'axios'
+
 export default function Home() {
+  const [posts,setPosts]=useState([])
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+     const res=await axios.get("/posts")
+      setPosts(res.data)
+    }
+    fetchPosts()
+
+  },[])
   return (
     <>
     <Header/>
     <div className="home">
-    <Posts/>
+    <Posts posts={posts}/>
     <Sidebar/>
     </div>
     </>
