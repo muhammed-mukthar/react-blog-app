0d4afb5569de45e0343d744fd011460f9d9bcee0
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-14 00:21:49 +0530

    register api connected to front end

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,8 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { useState } from "react"
-import "./sidebar.css"
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
 
 export default function Sidebar() {
   const [cats, setCats] = useState([]);
@@ -15,32 +16,36 @@ export default function Sidebar() {
   }, []);
   return (
     <div className="sidebar">
-        <div className="sidebarItem">
-            <div className="sidebarTitle">ABOUT ME</div>
-            <img src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iure nam debitis facilis facere, inventore qui.</p>
-
-        </div>
-        <div className="sidebarItem">
-            <div className="sidebarTitle">CATEGORIES</div>
-            <ul className="sidebarList">
-
-            {cats.map((c) => (
-           
+      <div className="sidebarItem">
+        <div className="sidebarTitle">ABOUT ME</div>
+        <img
+          src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iure
+          nam debitis facilis facere, inventore qui.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <div className="sidebarTitle">CATEGORIES</div>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
               <li className="sidebarListItem">{c.name}</li>
-           
+            </Link>
           ))}
-          
         </ul>
-        </div>
-        <div className="sidebarItem">
+      </div>
+      <div className="sidebarItem">
         <div className="sidebarTitle">FOLLOW US</div>
         <div className="sidebarSocial">
-        <i className="SideBarIcon  fa-brands fa-square-facebook"></i>
-        <i className="SideBarIcon  fa-brands fa-square-twitter"></i>
-        <i className="SideBarIcon  fa-brands fa-square-pinterest"></i>
-        <i className="SideBarIcon  fa-brands fa-square-instagram"></i> </div>
+          <i className="SideBarIcon  fa-brands fa-square-facebook"></i>
+          <i className="SideBarIcon  fa-brands fa-square-twitter"></i>
+          <i className="SideBarIcon  fa-brands fa-square-pinterest"></i>
+          <i className="SideBarIcon  fa-brands fa-square-instagram"></i>{" "}
         </div>
+      </div>
     </div>
-  )
+  );
 }
