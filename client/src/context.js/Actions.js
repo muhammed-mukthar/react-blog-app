cfcbc7fb63ab94d865e98404071bd900d4cdf373
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-14 10:14:12 +0530

    context created for authentication

---
+++ client/src/context.js/Actions.js
@@ -0,0 +1,14 @@
+export const LoginStart=(userCredential)=>({
+    type:"LOGIN_START"
+})
+
+
+export const LoginSuccess=(user)=>({
+    type:"LOGIN_SUCCESS",
+    payload:user,
+})
+
+
+export const LoginFailure=()=>({
+    type:"LOGIN_FAILURE"
+})
--- client/src/pages/homepage/Home.jsx
+++ client/src/pages/homepage/Home.jsx
@@ -12,6 +12,7 @@ export default function Home() {
   const {search}=useLocation();
 
 
+  
   useEffect(()=>{
     const fetchPosts=async()=>{
      const res=await axios.get("/posts"+search)
