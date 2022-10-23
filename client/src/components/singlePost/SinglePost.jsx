6415b442f793d34a31e4c866a0546ad05951bd8e
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-23 12:59:23 +0530

    delete property added

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,20 +1,34 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect ,useContext} from "react";
+import Axios from "axios";
 import { Link, useLocation } from "react-router-dom";
 import "./Singlepost.css";
 import { useState } from "react";
+import {Context} from '../../context/Context'
 const PF='http://localhost:5000/images/'
+
+
 export default function SinglePost() {
+  const { user } = useContext(Context);
   const location =useLocation()
  const path=location.pathname.split('/')[2];
  useEffect(()=>{
   const getPost=async()=>{
-    const res=await axios.get('/posts/'+path)
+    const res= await Axios.get(`/posts/`+path)
     setPost(res.data)
   }
   getPost()
  },[path])
  const [post,setPost]=useState({})
+
+const hadleDelete=async()=>{
+  try{
+ await Axios.delete(`/posts/${post._id}`,{data:{username:user.username}})
+ window.location.replace("/");
+
+  }catch(err){ }
+}
+
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
@@ -25,10 +39,11 @@ export default function SinglePost() {
         />}
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostedit">
+
+         {post.username ===user.username && <div className="singlePostedit">
             <i className="SinglePostIcon  fa-regular fa-pen-to-square"></i>
-            <i className="SinglePostIcon  fa-solid fa-trash"></i>
-          </div>
+            <i className="SinglePostIcon  fa-solid fa-trash" onClick={hadleDelete}></i>
+          </div>}
         </h1>
         <div className="singlePostinfo">
           <span className="SinglePostAuthor">
